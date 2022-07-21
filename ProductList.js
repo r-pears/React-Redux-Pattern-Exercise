6a58d2cbd8a304cce75f2332223a5225994e81ad
Ryan <ryjen.pears@gmail.com>
2022-07-21 09:20:06 +0200

    product list added

---
+++ ProductList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, shallowEqual } from 'react-redux';
+import CartIcons from './CartIcons';
+
+/** Show page with list of products.
+ * 
+ * Route / -> ProductList -> CartIcons
+ */
+function ProductList() {
+  const products = useSelector((store) => store.products, shallowEqual);
+  const productCards = Object.keys(products).map(id => (
+    <div className='col-md-3 mb-3' key={id}>
+      <div className='card'>
+        <div className='card-body'>
+          <h2 className='card-title text-center'>
+            <Link to={`/products/${id}`}>{products[id].name}</Link>
+          </h2>
+          <CartIcons id={id} />
+        </div>
+      </div>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Check out our all our awesome products!</h1>
+      <div className='row'>{productCards}</div>
+    </div>
+  )
+}
+
+export default ProductList;
